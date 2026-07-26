7e354118d9f35695ac680fdee9f7545b6f973e2a
sooriyansh <sooriyashpatel@gmail.com>
2026-07-26 11:53:46 +0530

    smal change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  distDir: ".next-build",
   serverExternalPackages: ["pdf-parse"],
   turbopack: {
     root: process.cwd(),
